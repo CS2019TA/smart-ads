45c05e3b7b2b02e4e403f6c36188d8b984f90c42
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-03-12 13:53:28 +0700

    add inference process

--- fog/jetson/inference/inference.py
+++ fog/jetson/inference/inference.py
@@ -1,41 +1,58 @@
 import asyncio
+import torch
 
 from fogverse import Producer, Consumer, ConsumerStorage
 from fogverse.logging.logging import CsvLogging
-from fogverse.util import get_header, numpy_to_base64_url
+
+WEIGHT = '/Users/WorkPlace/Documents/final_assignment/yolov5/crowdhuman.pt'
 
 class MyStorage (Consumer, ConsumerStorage):
     def __init__(self, keep_messages=False):
-        self.consumer_servers = '192.168.1.18'
+        self.consumer_servers = '192.168.1.9'
         self.consumer_topic = ['fog-input']
         Consumer.__init__(self)
         ConsumerStorage.__init__(self, keep_messages=keep_messages)
 
-class MyProducer (Producer, CsvLogging):
+class MyFogInference (Producer, CsvLogging):
     def __init__(self, consumer):
         self.consumer = consumer
         self.producer_topic = 'result'
-        self.producer_servers = '192.168.1.18'
+        self.producer_servers = '192.168.1.9'
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', WEIGHT)
         CsvLogging.__init__(self)
         Producer.__init__(self)
 
     async def receive(self):
         return await self.consumer.get()
 
-    # process your image here
-    async def process(self, data):
-        if (str(data) != ''):
-            print("not null")
-        else:
-            print("null")
+    def _process(self, data):
+        self.model.classes = 1
+        inference_results = self.model(data)
+        try:
+            head = inference_results.pandas().xyxy[0].value_counts('name').sort_index()[0]
+        except IndexError:
+            head = 0
 
-        return data
+        try:
+            person = inference_results.pandas().xyxy[0].value_counts('name').sort_index()[1]
+        except IndexError:
+            person = 0
+
+        final_result = str({"head" : head, "person" : person})
+        return final_result
+
+    async def process(self, data):
+        print (self.message.headers)
+        return await self._loop.run_in_executor(None,
+                                               self._process,
+                                               data)
 
     async def send(self, data):
-        pass
+        headers = self.message.headers
+        await super().send(data, headers=headers)
 
 async def main():
-    _Consumer, _Producer = (MyStorage, MyProducer)
+    _Consumer, _Producer = (MyStorage, MyFogInference)
     consumer = _Consumer()
     producer = _Producer(consumer)
     tasks = [consumer.run(), producer.run()]
@@ -47,4 +64,8 @@ async def main():
             t.close()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    finally:
+        loop.close()
