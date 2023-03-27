84d1b18fbbc4dc19e5401dfca5cf0cb2ae6cd2d7
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-03-27 20:32:52 +0700

    change ip address

--- decider/device_decider.py
+++ decider/device_decider.py
@@ -5,7 +5,7 @@ from fogverse.logging import CsvLogging
 
 class MyStorage(Consumer, ConsumerStorage):
     def __init__(self):
-        self.consumer_servers = '192.168.1.101'
+        self.consumer_servers = '192.168.1.3'
         self.consumer_topic = ['input', 'cpu-utilization']
         Consumer.__init__(self)
         ConsumerStorage.__init__(self)
@@ -14,7 +14,7 @@ class DeviceDecider (Producer, CsvLogging):
     def __init__(self, consumer, loop=None):
         self.consumer = consumer
         self.producer_topic = 'fog-input'
-        self.producer_servers = '192.168.1.101'
+        self.producer_servers = '192.168.1.3'
         self.forwarded_data = ''
         CsvLogging.__init__(self)
         Producer.__init__(self, loop=loop)
