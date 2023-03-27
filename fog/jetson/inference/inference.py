44519630bb9d9db1d96cff0d5cdbf9d8444b3b8a
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-03-27 18:56:29 +0700

    change index to docker container

--- fog/jetson/inference/inference.py
+++ fog/jetson/inference/inference.py
@@ -29,7 +29,7 @@ class MyFogInference (Producer, CsvLogging):
         self.consumer = consumer
         self.producer_topic = 'result'
         self.producer_servers = '192.168.1.101'
-        self.model = torch.hub.load(MODEL[1]["yolo"], 'custom', path=MODEL[1]["weight"], source='local', device=0, force_reload=True)
+        self.model = torch.hub.load(MODEL[0]["yolo"], 'custom', path=MODEL[0]["weight"], source='local', device=0, force_reload=True)
         CsvLogging.__init__(self)
         Producer.__init__(self)
 
