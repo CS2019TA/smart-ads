a595c9fcbba5a2de8f5ee7d73e087e579dd0c2a1
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-04-04 21:52:04 +0700

    change build configuratioon

--- fog/raspberry/ci.sh
+++ fog/raspberry/ci.sh
@@ -1,5 +1,5 @@
 docker buildx build \
     --platform linux/arm64 \
-    -t fxdros/fogverse-smart-ads:preprocess \
+    -t fxdros/fogverse-smart-ads:preprocess-jetson \
     -f fog/raspberry/Dockerfile \
     --push .
