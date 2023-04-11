18a4ba63f56483f349a3ee4043ffa6b15d145782
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-04-11 19:36:00 +0700

    change topic

--- fog/decider/ads_decider.py
+++ fog/decider/ads_decider.py
@@ -6,16 +6,15 @@ from fogverse.logging import CsvLogging
 
 class MyStorage(Consumer, ConsumerStorage):
     def __init__(self):
-        self.consumer_servers = ['152.118.25.199']
-        self.consumer_topic = ['result']
+        self.consumer_servers = '192.168.1.17'
+        self.consumer_topic = ['fog-result', 'cloud-result']
         Consumer.__init__(self)
         ConsumerStorage.__init__(self)
 
 class MyAdsDecider(Producer, CsvLogging):
     def __init__(self, consumer, loop=None):
         self.consumer = consumer
-        self.producer_topic = 'ads'
-        self.producer_servers = '152.118.25.199'
+        self.producer_servers = '192.168.1.17'
         CsvLogging.__init__(self)
         Producer.__init__(self, loop=loop)
 
@@ -25,7 +24,6 @@ class MyAdsDecider(Producer, CsvLogging):
     async def process(self, data):
         data = str(data).replace("\'", "\"")
         inference_dict = json.loads(data)
-        # TODO: implement decision logic
         video = ''
         if (inference_dict["head"] >= 1):
             video = '1'
@@ -35,6 +33,7 @@ class MyAdsDecider(Producer, CsvLogging):
         return video
 
     async def send(self, data):
+        self.producer_topic = self.message.topic
         headers = self.message.headers
         await super().send(data, headers=headers)
 
