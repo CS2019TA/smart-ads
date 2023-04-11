e72d7863c22fb7340f6e72aafd183030c1148fe4
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-04-11 19:30:25 +0700

    add result forwarder

---
+++ fog/forwarder/result_forwarder.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from fogverse import Producer, Consumer, ConsumerStorage
+from fogverse.logging import CsvLogging
+
+class MyStorage(Consumer, ConsumerStorage):
+    def __init__(self):
+        self.consumer_servers = '192.168.1.5'
+        self.consumer_topic = 'cloud-result'
+        Consumer.__init__(self)
+        ConsumerStorage.__init__(self)
+
+class MyInputForwarder(Producer, CsvLogging):
+    def __init__(self, consumer, loop=None):
+        self.consumer = consumer
+        self.producer_servers = '192.168.1.17'
+        CsvLogging.__init__(self)
+        Producer.__init__(self, loop=loop)
+
+    async def receive(self):
+        return await self.consumer.get()
+
+    async def send(self, data):
+        self.producer_topic = self.message.topic
+        headers = self.message.headers
+        await super().send(data, headers=headers)
+
+async def main():
+    _Consumer, _Producer = (MyStorage, MyInputForwarder)
+    consumer = _Consumer()
+    producer = _Producer(consumer)
+    tasks = [consumer.run(), producer.run()]
+
+    try:
+        await asyncio.gather(*tasks)
+    except:
+        for t in tasks:
+            t.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
