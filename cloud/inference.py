368767febcf44b7b122944881727c6f348051061
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-04-12 13:47:15 +0700

    test connection

--- cloud/inference.py
+++ cloud/inference.py
@@ -30,7 +30,7 @@ class MyFogInference (Producer, CsvLogging):
         self.consumer = consumer
         self.producer_topic = 'cloud-result'
         self.producer_servers = '192.168.1.5' # cloud kafka
-        self.model = torch.hub.load(MODEL[1]["yolo"], 'custom', path=MODEL[1]["weight"],
+        self.model = torch.hub.load(MODEL[0]["yolo"], 'custom', path=MODEL[1]["weight"],
                                     source='local', force_reload=True)
         CsvLogging.__init__(self)
         Producer.__init__(self)
@@ -40,27 +40,33 @@ class MyFogInference (Producer, CsvLogging):
 
     def _process(self, data):
         # revert preprocess
+
+
         data = cv2.resize(data, None, fx=2, fy=2)
 
-        # image inference
-        self.model.classes = 1
-        inference_results = self.model(data)
+        # # image inference
+        # self.model.classes = 1
+        # inference_results = self.model(data)
 
-        # get inference result
-        self.model.classes = 1
-        inference_results = self.model(data)
-        try:
-            head = inference_results.pandas().xyxy[0].value_counts('name').sort_index()[0]
-        except IndexError:
-            head = 0
+        # # get inference result
+        # self.model.classes = 1
+        # inference_results = self.model(data)
+        # try:
+        #     head = inference_results.pandas().xyxy[0].value_counts('name').sort_index()[0]
+        # except IndexError:
+        #     head = 0
 
-        try:
-            person = inference_results.pandas().xyxy[0].value_counts('name').sort_index()[1]
-        except IndexError:
-            person = 0
+        # try:
+        #     person = inference_results.pandas().xyxy[0].value_counts('name').sort_index()[1]
+        # except IndexError:
+        #     person = 0
 
-        final_result = str({"head" : head, "person" : person})
-        return final_result
+        # final_result = str({"head" : head, "person" : person})
+
+
+        # return final_result
+        print(data)
+        return data
 
     async def process(self, data):
         return await self._loop.run_in_executor(None,
