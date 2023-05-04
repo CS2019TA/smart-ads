797e89805738b5127fdd0b7e9e8b6914a43d4953
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-05-04 19:40:27 +0700

    change ip address

--- client/ads.py
+++ client/ads.py
@@ -4,7 +4,7 @@ import json
 from fastapi import APIRouter
 from aiokafka import AIOKafkaConsumer
 
-KAFKA_BOOTSTRAP_SERVERS= "10.119.81.52"
+KAFKA_BOOTSTRAP_SERVERS= "192.168.1.22"
 KAFKA_TOPIC= "ads"
 KAFKA_CONSUMER_GROUP= "client"
 
