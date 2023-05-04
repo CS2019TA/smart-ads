a0189c3cfdfe4e0074532b6b598d0a4c296c808c
Work Place <57487697+FXDROS@users.noreply.github.com>
2023-05-04 20:25:38 +0700

    add timestamp

--- fog/cpu_logger/logger.py
+++ fog/cpu_logger/logger.py
@@ -1,11 +1,16 @@
 import csv
 import psutil
 import time
+import datetime
 
 with open('cpu_utilization.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
 
+    writer.writerow(["timestamp", "cpu_utilization"])
     while (True):
-        cpu = [psutil.cpu_percent()]
-        writer.writerow(cpu)
+        timestamp = datetime.datetime.now()
+        cpu = psutil.cpu_percent()
+
+        data = [timestamp, cpu]
+        writer.writerow(data)
         time.sleep(1/10)
